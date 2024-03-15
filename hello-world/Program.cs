73eb97647a0f171f10b637c6943647e3128e3654
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-15 14:11:07 -0300

    add new updates

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace HelloWorld;
+﻿namespace hello_world;
 
 class Program
 {
---
+++ variables/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string cat = "Lola";
+        var date = DateTime.Now;
+        Console.WriteLine($"Hello, {cat}! Today is {date.DayOfWeek}, it's {date:HH:mm} now.");
+
+        int myNum = 5;               // Integer (whole number)
+        double myDoubleNum = 5.99D;  // Floating point number
+        float pi = 3.1415f;          // Floating point number
+        char myLetter = 'D';         // Character
+        bool myBool = true;          // Boolean
+        string myText = "Hello";     // String
+        Console.WriteLine($"My integer number is {myNum}, my floating-point number is {myDoubleNum}, the value of pi is approximately {pi}, the letter I represent is {myLetter}, and my text is {myText}. My boolean is {myBool}.");
+    }
+}
