e98f1933d34d16e35032bcb74d6d0fd8ad105e11
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-19 16:59:30 -0300

    add new updates

---
+++ 05-conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _05_conversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        // Explicit Conversion
+        double numDecimal1 = 10.5;
+        int numInt1 = (int)numDecimal1; 
+        Console.WriteLine(numInt1);
+
+        // Implicit Conversion
+        int numInt2 = 10;
+        double numDecimal2 = numInt2;
+        Console.WriteLine(numDecimal2); 
+    }
+}
