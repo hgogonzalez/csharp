febf32d481cd32701c2db85a267ffacabe782e5e
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-20 20:06:02 -0300

    :tada: add updates

---
+++ 10-loops/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _10_loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // for
+        Console.WriteLine("---- for ----");
+        for (int i = 0; i <= 3; i++)
+        {
+            Console.WriteLine(i);
+        }
+        
+        // foreach
+        Console.WriteLine("---- foreach ----");
+        List<int> fibNumbers = [0, 1, 1, 2, 3, 5, 8, 13];
+        foreach (int element in fibNumbers)
+        {
+            Console.WriteLine($"{element} ");
+        }
+        
+        // while
+        Console.WriteLine("---- while ----");
+        int n = 0;
+        while (n < 5)
+        {
+            Console.WriteLine(n);
+            n++;
+        }
+        
+        
+        // await foreach
+        // await foreach (var item in GetNumbersAsync())
+        // {
+        //   Console.WriteLine(item);
+        // }
+    }
+    
+    
+}
