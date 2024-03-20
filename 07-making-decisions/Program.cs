c5a5535899b6910e0e93fabb6427ac6d7dd31c79
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-20 19:32:06 -0300

    :tada: add new updates

--- 07-making-decisions/Program.cs
+++ 07-making-decisions/Program.cs
@@ -4,55 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        int vida = 3;
-        bool estaVivo = true;
-        string nombreGato = "Lola";
+        bool isAdmin = false;
+        bool isRegistered = true;
+        string userName = "tartaruga";
 
-        if (estaVivo)
+        if (isRegistered && userName != "" && userName.Equals("Admin"))
         {
-            vida++;
-            Console.WriteLine($"{nombreGato} ha ganado una vida");
-            Console.WriteLine($"La vida de {nombreGato} aumento a: {vida}");
+            Console.WriteLine($"Hi everyone, registered user");
+            Console.WriteLine($"Hi there, {userName}");
+            Console.WriteLine($"Hi there, Admin");
+
         }
-        else
+
+        if (isAdmin || isRegistered)
         {
-            vida--;
-            Console.WriteLine($"{nombreGato}  ha perdido una vida");
-            Console.WriteLine($"La vida de {nombreGato} disminuyó a: {vida}");
+            Console.WriteLine($"{userName} you're logged in");
         }
-
-        if (vida >=3) {
-            vida += 5;
-            Console.WriteLine($"{nombreGato} se ha transformando en SuperCat");
-            Console.WriteLine($"La vida de {nombreGato} aumento a: {vida}");
-            Console.WriteLine($"{nombreGato} tiene {vida} vidas");
-        } else {
-            Console.WriteLine($"{nombreGato} aun no puede transformarse en SuperCat");
-        }
-
-        TryParse tryParse = new TryParse();
-        tryParse.Main();
     }
 }
 
-public class TryParse
-{
-    public void Main()
-    {
-        string[] values = { null, "160519", "9432.0", "16,667", "   -322   ", "+4302", "(100);", "01FA" };
-        foreach (var value in values)
-        {
-            int number;
 
-            bool success = int.TryParse(value, out number);
-            if (success)
-            {
-                Console.WriteLine($"Converted '{value}' to {number}.");
-            }
-            else
-            {
-                Console.WriteLine($"Attempted conversion of '{value ?? "<null>"}' failed.");
-            }
-        }
-    }
-}
