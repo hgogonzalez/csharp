5eea99707faae30f1429548392aa39c5ff4b778b
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-20 19:46:29 -0300

    :tada: add updates

---
+++ 09-switch-statement/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _09_switch_statement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int option = 2;
+
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Option 1 selected");
+                break;
+            case 2:
+                Console.WriteLine("Option 2 selected");
+                break;
+            case 3:
+                Console.WriteLine("Option 3 selected");
+                break;
+            default:
+                Console.WriteLine("Option not recognized");
+                break;
+        }
+    }
+}
