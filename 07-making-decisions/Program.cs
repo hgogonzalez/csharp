2342088de1be0225724f5c57e3e2cc4e3383e04d
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-20 18:44:51 -0300

    add new file

--- 07-making-decisions/Program.cs
+++ 07-making-decisions/Program.cs
@@ -20,5 +20,14 @@ class Program
             Console.WriteLine($"{nombreGato}  ha perdido una vida");
             Console.WriteLine($"La vida de {nombreGato} disminuyó a: {vida}");
         }
+
+        if (vida >=3) {
+            vida += 5;
+            Console.WriteLine($"{nombreGato} se ha transformando en SuperCat");
+            Console.WriteLine($"La vida de {nombreGato} aumento a: {vida}");
+            Console.WriteLine($"{nombreGato} tiene {vida} vidas");
+        } else {
+            Console.WriteLine($"{nombreGato} aun no puede transformarse en SuperCat");
+        }
     }
 }
