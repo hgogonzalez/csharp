ae677453b607be69abc0ba02a79869437fabb8b9
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-22 12:56:58 -0300

    :tada: add new updates

---
+++ 02-multiple-constructor/Main.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_oop_intro;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person vittorio = new Person("Vittorio Bianchi", "Italy");
+        vittorio.Hello();
+        vittorio.DisplayLocation();
+
+        Person leah = new Person("Leah Goldman", "Polska");
+        leah.Hello();
+        leah.DisplayLocation();
+
+        Person lewi = new Person("Lewi Lewandowski", "Polska");
+        lewi.Hello();
+        lewi.DisplayLocation();
+
+        Person binyamin = new Person();
+        binyamin.Hello();
+        binyamin.DisplayLocation();
+    }
+}
---
+++ 02-multiple-constructor/Person.cs
@@ -0,0 +1,31 @@
+public class Person
+{
+    // Properties
+    public string Name { get; set; }
+    public string Country { get; set; }
+
+    // Constructor
+    public Person(string name, string country)
+    {
+        Name = name;
+        Country = country;
+    }
+
+    public Person()
+    {
+        Name = "Binyamin";
+        Country = "Hoffmann";
+    }
+
+    // Method
+    public void Hello()
+    {
+        Console.WriteLine($"Hello, I'm {Name} from {Country}");
+    }
+
+    // Method
+    public void DisplayLocation()
+    {
+        Console.WriteLine($"{Name} live in {Country}");
+    }
+}
