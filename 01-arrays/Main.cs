ce0f8377597c08e3942e207a2aa37856b622b1d8
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-25 01:00:15 -0300

    :tada: add new updates

--- 01-arrays/Main.cs
+++ 01-arrays/Main.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] grades = [1, 2, 3];
+        for (int i = 0; i < grades.Length; i++)
+        {
+            Console.WriteLine(grades[i]);            
+        }
+        
     }
 }
