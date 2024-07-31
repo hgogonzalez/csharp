cf4b0d4d46c30905b863e67f6d85da5deae78e51
hogonzalez <hugo.orlando2050@gmail.com>
2024-07-31 17:13:12 -0300

    variables added

---
+++ 01-intro-csharp/02-Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿// Variable declarations
+int integerNumber = 10;         // Integer variable
+double decimalNumber = 20.5;    // Double variable
+char character = 'A';           // Character variable
+string textString = "Hello";    // String variable
+bool isTrue = true;             // Boolean variable
+
+// Use of the variables
+Console.WriteLine("Integer number: " + integerNumber);
+Console.WriteLine("Decimal number: " + decimalNumber);
+Console.WriteLine("Character: " + character);
+Console.WriteLine("Text string: " + textString);
+Console.WriteLine("Boolean: " + isTrue);
+
+// Modification of the variables
+integerNumber = 25;
+decimalNumber = 50.75;
+character = 'B';
+textString = "Goodbye";
+isTrue = false;
+
+// Use of the modified variables
+Console.WriteLine("Modified integer number: " + integerNumber);
+Console.WriteLine("Modified decimal number: " + decimalNumber);
+Console.WriteLine("Modified character: " + character);
+Console.WriteLine("Modified text string: " + textString);
+Console.WriteLine("Modified boolean: " + isTrue);
